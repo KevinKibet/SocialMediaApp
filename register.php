4da1a99d2936cba6ad0c3151a89c3bd433ca63ed
KevinKibet <kevinkipkosgei310@gmail.com>
2020-09-12 13:55:28 +0300

    registration form

---
+++ register.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+</head>
+<body>
+   <form>
+   	<input type="text" name="fname" placeholder="First Name" required>
+   	<br>
+   	<input type="text" name="lname" placeholder="Last Name" required>
+   	<br>
+   	<input type="email" name="email" placeholder="Email" required>
+   	<br>
+   	<input type="password" name="password" placeholder="Password" required>
+   	<br>
+   	<input type="password" name="cpassword" placeholder="Confirm Password" required>
+   	<br>
+   	<input type="submit" name="submit" value="Register">
+
+   </form>
+</body>
+</html>
