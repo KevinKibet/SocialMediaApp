be13720a3093b614b0ab22a6c203ad5ee97c0ae4
KevinKibet <kevinkipkosgei310@gmail.com>
2020-09-13 18:04:45 +0300

    Adding error messages in an array

--- register.php
+++ register.php
@@ -69,32 +69,32 @@ if(isset($_POST['register'])){
    
 
 	}else{
-		echo "Emails Does not Match";
+		array_push($error_array, "Emails Does not Match") ;
 	}
 
 
 
     if(strlen($fname) > 25 || strlen($fname) < 2) {
-		echo "Your first name must be between 2 and 25 characters";
+		array_push($error_array, "Your first name must be between 2 and 25 characters");
 	}
 
 	if(strlen($lname) > 25 || strlen($lname) < 2) {
-		echo "Your last name must be between 2 and 25 characters";
+		array_push($error_array, "Your last name must be between 2 and 25 characters");
 	}
 
 	
 	if($password != $password2) {
-		echo  "Your passwords do not match";
+		array_push($error_array, "Your passwords do not match");
 	}
 	else {
 		if(preg_match('/[^A-Za-z0-9]/', $password)) {
-			echo "Your password can only contain english characters or numbers <br>";
+			array_push($error_array, "Your password can only contain english characters or numbers <br>");
 		}
 	}
 
 
     if(strlen($password > 30 || strlen($password) < 5)) {
-		echo "Your password must be betwen 5 and 30 characters";
+		array_push($error_array, "Your password must be betwen 5 and 30 characters");
 	}
 
 }
