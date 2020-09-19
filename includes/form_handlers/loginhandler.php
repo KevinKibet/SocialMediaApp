9410ab77821e247587b1a135dab1d3d3c0387204
KevinKibet <kevinkipkosgei310@gmail.com>
2020-09-19 10:42:17 +0300

    creating the navigation bar

--- includes/form_handlers/loginhandler.php
+++ includes/form_handlers/loginhandler.php
@@ -10,13 +10,13 @@ if (isset($_POST['loginbutton'])){
 	$_SESSION['loginemail'] = $email; //Store email into session variable 
 	$password = ($_POST['loginpassword']); //Get password
 
-	$check_database_query = mysqli_query($con, "SELECT email, password FROM users");
+	$check_database_query = mysqli_query($con, "SELECT email, password, username FROM users");
 	//$check_login_query = mysqli_num_rows($check_database_query);
 
 	while($row = mysqli_fetch_array($check_database_query)) {
 		if($row['email']== $email && $row['password'] == $password){
         session_start();
-        
+        $username = $row['username'];
         $user_closed_query = mysqli_query($con, "SELECT * FROM users WHERE email='$email' AND userclosed='yes'");
 		if(mysqli_num_rows($user_closed_query) == 1) {
 			$reopen_account = mysqli_query($con, "UPDATE users SET userclosed='no' WHERE email='$email'");
--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,10 @@ require "config/config.php" ;
 
 if (isset($_SESSION['username'])) {
 	$userLoggedin = $_SESSION['username'] ;
+	$user_detail_query = mysqli_query($con, "SELECT * FROM users WHERE username='$userLoggedin'");
+	$user = mysqli_fetch_array($user_detail_query);
 }else{
-	header("Location: register.php");
+	header("Location: index.php");
 }
 
 
@@ -14,7 +16,33 @@ if (isset($_SESSION['username'])) {
 <html>
 <head>
 	<title>Welcome to chat Nation</title>
+
+	<!--Java Script-->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
+
+	<!---css-->
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/fontawesome.min.css">
 </head>
+
+<div class = top_bar>
+	<div class="logo">
+		<a href=""> ChatNation</a>
+	</div>
+    <nav>
+    	<a href="#"><?php echo $user['firstname']; ?></i></a>
+    	<a href="#"><i class = "fa fa-home"></i></a>
+    	<a href="#"><i class = "fa fa-envelope"></i></a>
+    	<a href="#"><i class = "fa fa-bell-o"></i></a>
+    	<a href="#"><i class = "fa fa-users"></i></a>
+    	<a href="#"><i class = "fa fa-cog"></i></a>
+    	
+    </nav>
+</div>
+
+
 <body> 
 gsgs
